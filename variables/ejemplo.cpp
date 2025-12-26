522cb2f41b0c32f01fb78d8d1a15bec7dc4ce368
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-12-26 08:07:20 -0500

    variables_c++
    variables_c++

---
+++ variables/ejemplo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  // Incluye la librería para entrada y salida de datos (cout, cin)
+
+int main()           // Función principal donde inicia el programa
+{
+    int numero;      // Declaración de una variable entera llamada "numero"
+
+    numero = 20;     // Se asigna el valor 20 a la variable "numero"
+
+    std::cout << numero; // menor que Muestra en pantalla el valor de la variable "numero"
+
+    return 0;        // Indica que el programa terminó correctamente
+}
